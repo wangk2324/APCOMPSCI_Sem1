a5c629cb363d1349d7e3ce2e325b013d9c0d87fe
wangk2324 <kathywang2000@gmail.com>
2016-09-02 10:58:28 -0700

    Added Files

---
+++ APLesson_01/ASCII1.java
@@ -0,0 +1,12 @@
+public class ASCII1
+{
+	public static void main(String[]args)
+		{
+			System.out.println("                  ,---, ");
+			System.out.println("    _ __ _ /     \\| ");
+			System.out.println("  ;( )____  ,  ) ");
+			System.out.println(" ; //         '---;");
+			System.out.println("   \\          |  ^");
+			System.out.println("    ^       ^");
+		}
+}
---
+++ APLesson_01/ASCII2.java
@@ -0,0 +1,13 @@
+public class ASCII2
+{
+	public static void main(String[]args)
+		{
+			System.out.println(" __         __");
+			System.out.println("/   \\.-\"\"\"-./   \\");
+			System.out.println("\\    -   -        /");
+			System.out.println("  |   o   o   | ");
+			System.out.println("  \\  .-\'\'\'-.   /");
+			System.out.println(" \'-\\__Y__/-\' ");
+			System.out.println("        ---     ");
+		}
+}
